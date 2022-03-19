40048dc9aac1ab6166379e89f825be745c452a7e
marcelodosanjosc <marcelodosanjosc@gmail.com>
2022-03-19 17:17:34 -0400

    page

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -29,11 +29,15 @@ function Listing() {
             }
         )
 
-    }, [pageNumber])
+    }, [pageNumber]);
+
+    const handlePageChange = ( newPageNumber : number)=>{
+        setPageNumber(newPageNumber);
+    }
 
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange}/>
             <div className="container">
                 <div className="row">
                     {page.content.map(movie =>
